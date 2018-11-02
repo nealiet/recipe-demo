f589cbb763242d49aec84ad8d171d8d9b6dcd624
nealiet <nealiet@comcast.net>
2018-11-02 11:54:30 -0400

    Commit after Spring Initialization

---
+++ src/main/java/com/njt/recipedemo/recipedemo/RecipeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.njt.recipedemo.recipedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeDemoApplication.class, args);
+	}
+}
